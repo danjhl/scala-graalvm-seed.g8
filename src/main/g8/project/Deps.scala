0251dda84902293f5cb9cc196624cb79f0ab4494
danjhl <d4njhl@gmail.com>
2019-04-25 21:59:17 +0200

    change style

--- src/main/g8/project/Deps.scala
+++ src/main/g8/project/Deps.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object Deps {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
-  lazy val scribe = "com.outr" %% "scribe" % "2.7.3"
+  lazy val scribe    = "com.outr"      %% "scribe"    % "2.7.3"
 }
